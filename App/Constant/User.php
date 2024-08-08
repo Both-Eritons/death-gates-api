31d7a2c2313c5008b2faaedae3ea63f5fb2356de
Eriton <botheritons2005@gmail.com>
2024-08-08 19:45:09 -0300

    add User Constant

---
+++ App/Constant/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Constant;
+
+class User{
+  private array $messages = array(
+    "CREATED" => "Usuario Registrado!",
+    "DELETED" => "Usuario Deletado!",
+    "UPDATED" => "Usuario Atualizado!",
+    "EXISTS" => "Esse Usuario Ja Existe!",
+    "NOT_EXISTS" => "Esse Usuario Nao Existe",
+    "ERROR" => "Ocorreu Um Erro acom Esse Usuario!"
+  );
+}
