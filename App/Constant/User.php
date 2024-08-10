d47194c91887de97958510ba0141e8a6cf3f3606
Eriton <botheritons2005@gmail.com>
2024-08-10 11:25:04 -0300

    feat(UserConstant): refatorando pra melhor

--- App/Constant/User.php
+++ App/Constant/User.php
@@ -2,13 +2,11 @@
 
 namespace App\Constant;
 
-class User{
-  private array $messages = array(
-    "CREATED" => "Usuario Registrado!",
-    "DELETED" => "Usuario Deletado!",
-    "UPDATED" => "Usuario Atualizado!",
-    "EXISTS" => "Esse Usuario Ja Existe!",
-    "NOT_EXISTS" => "Esse Usuario Nao Existe",
-    "ERROR" => "Ocorreu Um Erro acom Esse Usuario!"
-  );
+trait User{
+  protected const CREATED = "Usuario Registrado!";
+  private const DELETED = "Usuario Deletado!";
+  private const UPDATED = "Usuario Atualizado!";
+  private const EXISTS = "Esse Usuario Ja Existe!";
+  private const NOT_EXISTS =  "Esse Usuario Nao Existe";
+  private const ERROR = "Ocorreu Um Erro acom Esse Usuario!";
 }
