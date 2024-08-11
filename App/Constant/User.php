f0aead82b2bda23e5c4366ce22a9e520c95414a1
Eriton <botheritons2005@gmail.com>
2024-08-11 10:30:45 -0300

    feat(UserConstant): Usuario Encontrado

--- App/Constant/User.php
+++ App/Constant/User.php
@@ -9,4 +9,5 @@ trait User{
   private const EXISTS = "Esse Usuario Ja Existe!";
   private const NOT_EXISTS =  "Esse Usuario Nao Existe";
   private const ERROR = "Ocorreu Um Erro acom Esse Usuario!";
+  private const FOUND = "Usuario Encontrado!";
 }
