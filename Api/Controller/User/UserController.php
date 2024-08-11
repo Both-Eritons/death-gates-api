97f9f183109ad426dfd0132b926890e93e650199
Eriton <botheritons2005@gmail.com>
2024-08-11 10:36:07 -0300

    chore: corrigindo algumas coisas...

--- Api/Controller/User/UserController.php
+++ Api/Controller/User/UserController.php
@@ -12,41 +12,40 @@ use Damianopetrungaro\PHPCommitizen\Section\Body;
 use Psr\Http\Message\ResponseInterface as Res;
 use Slim\Psr7\Request as Req;
 
-class UserController extends Json {
+class UserController extends Json
+{
   use  UserConstant;
   private UserService $user;
-  function __construct(UserService $user){
+  function __construct(UserService $user)
+  {
     $this->user = $user;
   }
 
-  function findUserById(Req $req, Res $res, array $args) {
+  function findUserById(Req $req, Res $res, array $args)
+  {
 
-    try{
+    try {
       $id = $args["id"];
       $user = $this->user->findUserById($id)->__toArray();
 
-      return $this->send($res, self::EXISTS, 200, $user);
-    } catch(NotFound $e) {
+      return $this->send($res, self::FOUND, 200, $user);
+    } catch (NotFound $e) {
       return $this->send($res, $e->getMessage(), 404);
     }
   }
 
-  function createUser(Req $req, Res $res) {
+  function createUser(Req $req, Res $res)
+  {
 
-    try{
+    try {
       $body = $req->getBody();
       $body = json_decode($body);
-
-      if(!$body->username && !$body->password && !$body->email)
-        return $this->send($res, "Preencha todos os campos...", 400);
-
+    
       $user = $this->user->createUser($body->username, $body->password, $body->email)->__toArray();
 
       return $this->send($res, self::CREATED, 200, $user);
-
-    } catch (UserExists|UserValidation $e) {
+    } catch (UserExists | UserValidation $e) {
       return $this->send($res, $e->getMessage(), $e->getCode());
     }
-
   }
 }
