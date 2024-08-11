60c04219749fad083aeae2e156c38cf11cbe80e1
Eriton <botheritons2005@gmail.com>
2024-08-11 06:10:38 -0300

    chore: removendo importação inuteis

--- public/index.php
+++ public/index.php
@@ -1,10 +1,6 @@
 <?php
 require_once '../vendor/autoload.php';
 
-use App\Helper\Json;
-use Slim\Factory\AppFactory;
-use Slim\Psr7\Request as Req;
-use Psr\Http\Message\ResponseInterface as Res;
 use App\Configuration\DotEnv;
 use App\Configuration\Slim;
 use App\Routes\Api;
