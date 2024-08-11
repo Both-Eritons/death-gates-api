d139832e2413bbece627015b7eec2d4100e2ba57
Eriton <botheritons2005@gmail.com>
2024-08-11 07:19:16 -0300

    feat: tamanho maximo e minimo de caracteres recebido

---
+++ App/Configuration/Constants/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Configuration\Constants;
+
+trait User{
+  public const USER_MIN_LENGTH = 6;
+  public const USER_MAX_LENGTH = 24;
+
+  public const PASS_MIN_LENGTH = 6;
+  public const PASS_MAX_LENGTH = 40;
+
+  public const MAIL_MIN_LENGTH = 40;
+  public const MAIL_MAX_LENGTH = 256;
+}
